e9dc7a2812bbf04a1306d8a354da5dbc8a65d7d0
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-03-01 09:58:13 +0100

    delete and settings button

--- components/signature-detail/content-edit/content-edit.tsx
+++ components/signature-detail/content-edit/content-edit.tsx
@@ -10,7 +10,16 @@ import { EmailEditContent } from "./email-edit-content";
 import { createClient } from "@/utils/supabase/client";
 import { base64ToFile } from "@/utils/base64ToFile";
 import { ImageEditContent } from "./image-edit-content";
+import Modal from "@/components/ui/modal";
 import { Typography } from "@/components/ui/typography";
+
+const SavingInfo = () => {
+  return (
+    <div className="flex  w-full pb-6 pt-6 justify-center">
+      <Typography variant="large">Saving... Please wait</Typography>
+    </div>
+  );
+};
 
 export const ContentEdit = (props: any) => {
   const { contentPathToEdit, signatureId } = props;
@@ -19,13 +28,35 @@ export const ContentEdit = (props: any) => {
   const [isSavingSignature, setIsSavingSignature] = useState(false);
 
   const supabase = createClient();
-  const { rows, setContent } = useSignatureStore();
+  const { rows, setContent, removeRow } = useSignatureStore();
 
   const { setContentEdit, contentEdit } = useContentEditStore();
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const path = `${contentPathToEdit}.content`;
   const content = get(rows, path);
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const deleteRow = useCallback(async () => {
+    setIsDeleting(true);
+
+    const saveData = async (rows: any) => {
+      await supabase.functions.invoke("patch-signature", {
+        method: "PATCH",
+        body: {
+          signatureId,
+          signatureContent: { rows },
+        },
+      });
+
+      setIsDeleting(false);
+      setIsDeleteModalOpen(false);
+    };
+
+    removeRow(contentPathToEdit, saveData);
+  }, [contentPathToEdit, removeRow, signatureId, supabase.functions]);
 
   useEffect(() => {
     if (!iniContent) {
@@ -34,6 +65,7 @@ export const ContentEdit = (props: any) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [content]);
 
+  // TODO - samostatná funkce
   const saveSignature = useCallback(async () => {
     if (
       content.type == ContentType.IMAGE &&
@@ -78,6 +110,8 @@ export const ContentEdit = (props: any) => {
       if (imageData?.imagePreviewPublicUrl) {
         const deepCopyRows = cloneDeep(rows);
 
+        // TODO - zkusit nastavit udefined místo prádzného stringu
+
         // remove all unnecessary data because we wont to save them into database
 
         const pathToImageSrc = `${contentPathToEdit}.content.components[0].src`;
@@ -86,6 +120,7 @@ export const ContentEdit = (props: any) => {
 
         const pathOriginalImagePreview = `${contentPathToEdit}.content.components[0].originalImagePreview`;
         set(deepCopyRows, pathOriginalImagePreview, "");
+        setContent(pathOriginalImagePreview, "");
 
         const pathToCropImagePreview = `${contentPathToEdit}.content.components[0].cropImagePreview`;
         set(deepCopyRows, pathToCropImagePreview, "");
@@ -112,6 +147,10 @@ export const ContentEdit = (props: any) => {
             signatureContent: { rows: deepCopyRows },
           },
         });
+
+        setContentEdit({
+          editPath: null,
+        });
       }
 
       return;
@@ -124,56 +163,109 @@ export const ContentEdit = (props: any) => {
         signatureContent: { rows },
       },
     });
+
+    setContentEdit({
+      editPath: null,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     content.components,
     content.type,
     contentPathToEdit,
     rows,
     setContent,
+    setContentEdit,
     signatureId,
-    supabase.functions,
   ]);
 
   return (
     <div key={path}>
       <div ref={wrapperRef}>
-        {<div className="pb-8">{getContentType(content, path)}</div>}
+        {!isSavingSignature && (
+          <div className="pb-8">{getContentType(content, path)}</div>
+        )}
       </div>
 
-      {isSavingSignature && (
-        <div className="flex  w-full pb-6 pt-6 justify-center">
-          <Typography variant="large">Saving... Please wait</Typography>
-        </div>
-      )}
+      {isSavingSignature && <SavingInfo />}
 
       {!isSavingSignature && !contentEdit.subEdit && (
-        <div className="flex flex-row w-full pb-6 pt-6 justify-between">
-          <Button
-            variant="outline"
-            onClick={() => {
-              setContent(path, iniContent);
-              setContentEdit({
-                editPath: null,
-              });
-            }}
-          >
-            Close
-          </Button>
-          {/*   TODO - modál pro nepřihlášené uživatele "Ukládat mohou pouze registrovaní uživatelé" */}
-          <Button
-            variant="blue"
-            size="md"
-            onClick={async () => {
-              await saveSignature();
-              setContentEdit({
-                editPath: null,
-              });
-            }}
-            loading={isSavingSignature}
-          >
-            {isSavingSignature ? "Saving..." : "Save"}
-          </Button>
-        </div>
+        <>
+          {content.components[0].cropImagePreview && (
+            <>
+              <div>
+                <Button
+                  variant="red"
+                  onClick={() => {
+                    setIsDeleteModalOpen(true);
+                  }}
+                >
+                  Delete
+                </Button>
+              </div>
+              <hr className="border-t border-gray-200 my-4" />
+            </>
+          )}
+
+          <Modal size="medium" isOpen={isDeleteModalOpen}>
+            {isDeleting && <SavingInfo />}
+
+            {!isDeleting && (
+              <div className="p-2">
+                <Typography variant="h3">Delete content</Typography>
+                <Typography variant="body">
+                  Are you sure you want to delete this content?
+                </Typography>
+                <div className="mt-8 flex justify-between">
+                  <Button
+                    variant="outline"
+                    onClick={() => {
+                      setIsDeleteModalOpen(false);
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    loading={isDeleting}
+                    variant="red"
+                    onClick={async () => deleteRow()}
+                  >
+                    {isDeleting ? "Deleting..." : "Delete"}
+                  </Button>
+                </div>
+              </div>
+            )}
+          </Modal>
+
+          <div className="flex flex-row w-full pb-6 pt-6 justify-between">
+            <Button
+              variant="outline"
+              onClick={() => {
+                setContent(path, iniContent);
+                setContentEdit({
+                  editPath: null,
+                });
+              }}
+            >
+              Close
+            </Button>
+            {/*   TODO - modál pro nepřihlášené uživatele "Ukládat mohou pouze registrovaní uživatelé" */}
+
+            {content.components[0].cropImagePreview && (
+              <Button
+                variant="blue"
+                size="md"
+                onClick={async () => {
+                  await saveSignature();
+                  setContentEdit({
+                    editPath: null,
+                  });
+                }}
+              >
+                Save
+              </Button>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
@@ -181,7 +273,7 @@ export const ContentEdit = (props: any) => {
 
 const getContentType = (content: any, contentPathToEdit: any) => {
   const type: ContentType = content?.type;
-  const { components } = content;
+  const components = content?.components;
 
   switch (type) {
     case ContentType.TEXT:
--- components/signature-detail/signature-edit-add.tsx
+++ components/signature-detail/signature-edit-add.tsx
@@ -1,47 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Fragment, useCallback, useState } from "react";
+import { Fragment } from "react";
 import { getContentView } from "./content-view/content-view";
 import { Button } from "@/components/ui/button";
 import { ContentEdit } from "@/components/signature-detail/content-edit/content-edit";
 import { useContentEditStore } from "./store/content-edit-add-path-store";
 import { ContentAdd } from "@/components/signature-detail/content-add/content-add";
 import { useParams } from "next/navigation";
-import { ContextMenu } from "../ui/context-menu";
-import Modal from "../ui/modal";
-import { Typography } from "../ui/typography";
-import { useSignatureStore } from "./store/content-edit-add-store";
-import { createClient } from "@/utils/supabase/client";
 
 export const EmailTemplateEdit = (props: any) => {
   const { rows } = props;
   const { setContentEdit, contentEdit } = useContentEditStore();
   const { id: signatureId } = useParams();
-
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [currentPath, setCurrentPath] = useState("");
-
-  const { removeRow } = useSignatureStore();
-  const supabase = createClient();
-
-  const deleteRow = useCallback(async () => {
-    setIsDeleting(true);
-
-    const saveData = async (rows: any) => {
-      await supabase.functions.invoke("patch-signature", {
-        method: "PATCH",
-        body: {
-          signatureId,
-          signatureContent: { rows },
-        },
-      });
-
-      setIsDeleting(false);
-      setIsDeleteModalOpen(false);
-    };
-
-    removeRow(currentPath, saveData);
-  }, [currentPath, removeRow, signatureId, supabase.functions]);
 
   const renderColumn = (column: any, path: string) => {
     const rowPath = `${path}.rows`;
@@ -51,6 +20,7 @@ export const EmailTemplateEdit = (props: any) => {
         style={{
           ...column.style,
           display: "table-cell",
+          verticalAlign: "middle",
         }}
       >
         <div
@@ -63,7 +33,10 @@ export const EmailTemplateEdit = (props: any) => {
         </div>
 
         {contentEdit.addPath !== rowPath && (
-          <div className="">
+          <div className="flex justify-between mt-2">
+            <Button variant="blue" size="sm">
+              Settings
+            </Button>
             <Button
               size="sm"
               variant="orange"
@@ -119,7 +92,7 @@ export const EmailTemplateEdit = (props: any) => {
         );
       }
 
-      const content = getContentView(row?.content, false);
+      const content = getContentView(row?.content);
       if (content) {
         return (
           <Fragment key={`tr-${row.id}`}>
@@ -141,37 +114,22 @@ export const EmailTemplateEdit = (props: any) => {
             </div>
 
             {contentEdit.editPath !== currentPath && (
-              <div className="mt-1 mb-3 flex justify-between">
-                <Button
-                  size="sm"
-                  variant="blue"
-                  onClick={() => {
-                    setContentEdit({
-                      editPath: currentPath,
-                      addPath: null,
-                    });
-                  }}
-                >
-                  Edit
-                </Button>
-                <ContextMenu>
-                  <div className="p-1 flex flex-col gap-2 items-start">
-                    <Button
-                      size="sm"
-                      variant="gray"
-                      onClick={() => {
-                        setIsDeleteModalOpen(true);
-                        setCurrentPath(currentPath);
-                      }}
-                    >
-                      Delete
-                    </Button>
-                    <Button size="sm" variant="gray" onClick={() => {}}>
-                      Settings
-                    </Button>
-                  </div>
-                </ContextMenu>
-              </div>
+              <>
+                <div className="mt-1 mb-4 flex justify-end">
+                  <Button
+                    size="sm"
+                    variant="blue"
+                    onClick={() => {
+                      setContentEdit({
+                        editPath: currentPath,
+                        addPath: null,
+                      });
+                    }}
+                  >
+                    Edit
+                  </Button>
+                </div>
+              </>
             )}
           </Fragment>
         );
@@ -260,34 +218,6 @@ export const EmailTemplateEdit = (props: any) => {
           signatureId={signatureId}
         />
       )}
-
-      {/* 
-                TODO: lepší popis a název modálu, co se bude mazat */}
-      <Modal size="medium" isOpen={isDeleteModalOpen}>
-        <div className="p-2">
-          <Typography variant="h3">Delete content</Typography>
-          <Typography variant="body">
-            Are you sure you want to delete this content?
-          </Typography>
-          <div className="mt-8 flex justify-between">
-            <Button
-              variant="outline"
-              onClick={() => {
-                setIsDeleteModalOpen(false);
-              }}
-            >
-              Cancel
-            </Button>
-            <Button
-              loading={isDeleting}
-              variant="red"
-              onClick={async () => deleteRow()}
-            >
-              {isDeleting ? "Deleting..." : "Delete"}
-            </Button>
-          </div>
-        </div>
-      </Modal>
     </>
   );
 };
