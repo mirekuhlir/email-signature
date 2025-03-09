e21e8d569de377d1eb82ca005a8643f5570b84a6
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-03-09 10:05:28 +0100

    refactor

--- src/components/templates-examples.tsx
+++ src/components/templates-examples.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { EmailTemplateView } from './signature-detail/content-view/signature-view';
 import { templates } from '@/src/templates';
-import { Container } from './ui/container';
 import { createClient } from '../utils/supabase/client';
 import { Button } from './ui/button';
 import { useRouter } from 'next/navigation';
@@ -16,38 +15,44 @@ export const TemplatesExamples = (props: TemplatesExamplesProps) => {
   const supabase = createClient();
 
   return (
-    <Container>
-      {templates.map((template, index) => (
-        <div key={index}>
-          <EmailTemplateView rows={template.rows} />
-
-          <Button
-            onClick={async () => {
-              if (isSignedIn) {
-                const { data } = await supabase.functions.invoke(
-                  'post-signature',
-
-                  {
-                    method: 'POST',
-
-                    body: {
-                      signatureContent: template,
-                    },
-                  },
-                );
-
-                router.push(`/signatures/${data.signatureId}`);
-              } else {
-                router.push(
-                  `/signatures/example/?template=${template.templateSlug}`,
-                );
-              }
-            }}
+    <div className="w-full">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {templates.map((template, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center p-4"
           >
-            Select
-          </Button>
-        </div>
-      ))}
-    </Container>
+            <div className="w-full max-w-md mx-auto">
+              <EmailTemplateView rows={template.rows} />
+            </div>
+            <div className="flex justify-center mt-4">
+              <Button
+                onClick={async () => {
+                  if (isSignedIn) {
+                    const { data } = await supabase.functions.invoke(
+                      'post-signature',
+                      {
+                        method: 'POST',
+                        body: {
+                          signatureContent: template,
+                        },
+                      },
+                    );
+
+                    router.push(`/signatures/${data.signatureId}`);
+                  } else {
+                    router.push(
+                      `/signatures/example/?template=${template.templateSlug}`,
+                    );
+                  }
+                }}
+              >
+                Select
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
