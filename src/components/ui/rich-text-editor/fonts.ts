d47a4aaa7c31edffcae4f58c8e73eb1fecf73a9d
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-03-17 20:56:28 +0100

    more font sizes

--- src/components/ui/rich-text-editor/fonts.ts
+++ src/components/ui/rich-text-editor/fonts.ts
@@ -31,44 +31,173 @@ export const FONTS = [
 
 export const FONT_SIZES = [
     {
+        value: "8",
+        label: "8px",
+    },
+    {
+        value: "9",
+        label: "9px",
+    },
+    {
+        value: "10",
+        label: "10px",
+    },
+    {
+        value: "11",
+        label: "11px",
+    },
+    {
         value: "12",
         label: "12px",
+    },
+    {
+        value: "13",
+        label: "13px",
     },
     {
         value: "14",
         label: "14px",
     },
     {
+        value: "15",
+        label: "15px",
+    },
+    {
         value: "16",
         label: "16px",
+    },
+    {
+        value: "17",
+        label: "17px",
     },
     {
         value: "18",
         label: "18px",
     },
     {
+        value: "19",
+        label: "19px",
+    },
+    {
         value: "20",
         label: "20px",
+    },
+    {
+        value: "21",
+        label: "21px",
+    },
+    {
+        value: "22",
+        label: "22px",
+    },
+    {
+        value: "23",
+        label: "23px",
     },
     {
         value: "24",
         label: "24px",
     },
     {
+        value: "25",
+        label: "25px",
+    },
+    {
+        value: "26",
+        label: "26px",
+    },
+    {
+        value: "27",
+        label: "27px",
+    },
+    {
         value: "28",
         label: "28px",
     },
     {
+        value: "30",
+        label: "30px",
+    },
+    {
+        value: "31",
+        label: "31px",
+    },
+
+    {
         value: "32",
         label: "32px",
+    },
+    {
+        value: "33",
+        label: "33px",
+    },
+    {
+        value: "34",
+        label: "34px",
+    },
+    {
+        value: "35",
+        label: "35px",
+    },
+    {
+        value: "36",
+        label: "36px",
+    },
+    {
+        value: "37",
+        label: "37px",
+    },
+    {
+        value: "38",
+        label: "38px",
+    },
+    {
+        value: "39",
+        label: "39px",
     },
     {
         value: "40",
         label: "40px",
     },
     {
+        value: "41",
+        label: "41px",
+    },
+    {
+        value: "42",
+        label: "42px",
+    },
+    {
+        value: "43",
+        label: "43px",
+    },
+    {
+        value: "44",
+        label: "44px",
+    },
+    {
+        value: "45",
+        label: "45px",
+    },
+    {
+        value: "46",
+        label: "46px",
+    },
+    {
+        value: "47",
+        label: "47px",
+    },
+    {
         value: "48",
         label: "48px",
+    },
+    {
+        value: "49",
+        label: "49px",
+    },
+    {
+        value: "50",
+        label: "50px",
     },
 ];
 
