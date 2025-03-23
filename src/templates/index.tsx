2209d36949dc0706b77137a24a91c76fac54bc8f
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-03-23 12:11:00 +0100

    templates

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,7 +1,8 @@
 import { signature_a } from './signature_a';
+import { signature_empty } from './signature_empty';
 
-export const templates = [signature_a];
+export const templates = [signature_a, signature_empty];
 
 export const getTemplateBySlug = (slug: string) => {
-  return templates.find((template) => template.info.templateSlug === slug);
+  return templates.find((template) => template?.info?.templateSlug === slug);
 };
