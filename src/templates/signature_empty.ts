c7227e9acdcb31ac5716eea19604f2d9bcf8e6c1
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-03-23 12:10:43 +0100

    signature_empty

---
+++ src/templates/signature_empty.ts
@@ -0,0 +1,11 @@
+export const signature_empty = {
+    info: {
+        templateSlug: "signature-empty",
+        version: "0.1",
+      },
+      colors: [
+        "rgb(0,148,47)",
+      ],
+      rows: [
+      ]
+};
