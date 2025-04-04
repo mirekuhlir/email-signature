6621da9a0411931cf659df143c8068f0ccc034d2
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-04-04 21:27:49 +0200

    signature sort

--- src/components/signatures-list.tsx
+++ src/components/signatures-list.tsx
@@ -261,8 +261,8 @@ export const SignaturesList = (props: any) => {
         {signatures
           ?.sort(
             (a, b) =>
-              new Date(b.created_at).getTime() -
-              new Date(a.created_at).getTime(),
+              new Date(b.updated_at || b.created_at).getTime() -
+              new Date(a.updated_at || a.created_at).getTime(),
           )
           .map((signature: any) => (
             <SignaturesPreview
