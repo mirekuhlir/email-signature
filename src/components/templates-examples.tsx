f88d83cf7fd7fb4fb77d4fc15057f6cdba6b6085
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-04-13 15:37:49 +0200

    refactor

--- src/components/templates-examples.tsx
+++ src/components/templates-examples.tsx
@@ -16,37 +16,32 @@ export const TemplatesExamples = (props: TemplatesExamplesProps) => {
   const router = useRouter();
 
   return (
-    <div className="w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        {templates.map((template, index) => (
-          <div key={index} className="flex flex-col mt-4 mb-4">
-            {/*    TODO - přidat nějakou minimální výšký a šířku? */}
-            <div className="min-h-[200px]">
-              <div className="mb-4">
-                <Typography variant="labelBase">
-                  {template.info?.name}
-                </Typography>
-              </div>
-              <EmailTemplateView rows={template.rows} />
+    <>
+      {templates.map((template, index) => (
+        <div key={index} className="flex flex-col mt-4 mb-20">
+          <div className="">
+            <div className="mb-4">
+              <Typography variant="labelBase">{template.info?.name}</Typography>
             </div>
-            <div className="flex justify-center mt-4">
-              <Button
-                onClick={async () => {
-                  if (isSignedIn) {
-                    createSignature(template);
-                  } else {
-                    router.push(
-                      `/signatures/example/?template=${template.info?.templateSlug}`,
-                    );
-                  }
-                }}
-              >
-                {'Select'}
-              </Button>
-            </div>
+            <EmailTemplateView rows={template.rows} />
           </div>
-        ))}
-      </div>
-    </div>
+          <div className="flex mt-4 justify-end sm:justify-start">
+            <Button
+              onClick={async () => {
+                if (isSignedIn) {
+                  createSignature?.(template);
+                } else {
+                  router.push(
+                    `/signatures/example/?template=${template.info?.templateSlug}`,
+                  );
+                }
+              }}
+            >
+              {'Select'}
+            </Button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
