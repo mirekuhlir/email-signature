c2da7bc0336862a70eb8e028e77cf0c0e55b441d
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-04-19 13:23:19 +0200

    refactor

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -48,9 +48,14 @@ const Modal: React.FC<ModalProps> = ({
         className={`bg-white rounded-lg shadow-lg ${sizeClasses[size]} ${size === 'fullscreen' ? 'h-full flex flex-col' : ''}`}
       >
         {title && (
-          <div className="flex justify-between items-center p-4 border-b">
-            <Typography variant="h4">{title}</Typography>
-          </div>
+          <>
+            <div className="flex justify-between items-center pt-4 px-4 sm:px-6">
+              <Typography variant="h4">{title}</Typography>
+            </div>
+            <div className="px-4 sm:px-6 py-2">
+              <Hr />
+            </div>
+          </>
         )}
         <div
           className={`overflow-auto ${size === 'fullscreen' ? 'flex-grow' : 'max-h-[80vh]'} ${isZeroPadding ? 'p-0' : 'p-4 sm:p-6'}`}
