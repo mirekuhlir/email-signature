bbfd5e8ef50c78fc883ef6aeca03b3bb8a298218
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-04-19 08:55:16 +0200

    templates update

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,11 +1,13 @@
 import { signature_a } from './signature_a';
 import { signature_b } from './signature_b';
+import { signature_c } from './signature_c';
 import { signature_empty_one_columns } from './signature_empty_one_columns';
 import { signature_empty_two_columns } from './signature_empty_two_columns';
 
 export const templates = [
   signature_a,
   signature_b,
+  signature_c,
   signature_empty_one_columns,
   signature_empty_two_columns,
 ];
