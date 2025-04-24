a0305cbbb4c3383012e59080e01b6e74fda02fd4
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-04-24 21:21:21 +0200

    refactor

--- src/components/ui/image-uploader-crop/image-uploader-crop.tsx
+++ src/components/ui/image-uploader-crop/image-uploader-crop.tsx
@@ -627,7 +627,7 @@ export default function ImageUploadCrop(props: ImageUploaderProps) {
 
   if (isLoadingOriginalImage) {
     return (
-      <div className="flex items-center justify-center w-full h-[70px]">
+      <div className="flex items-center justify-center w-full h-[70px] mt-5">
         <LoadingInfo text="Loading image. Please wait..." />
       </div>
     );
