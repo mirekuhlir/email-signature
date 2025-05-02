d91f40be5703c7c3a450885ead61d7e0ab890b76
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-05-02 18:28:29 +0200

    refactor templates

--- src/templates/signature_b.ts
+++ src/templates/signature_b.ts
@@ -41,7 +41,7 @@ export const signature_b = () => {
                   "type": "image",
                   "components": [
                     {
-                      "id": "hyqqp19",
+                      "id": generateRandomId(),
                       "src":
                         `https://${process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com/examples/signature_b_preview.png`,
                       "padding": "0px 0px 0px 0px",
