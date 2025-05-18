53e3bb6dddd2000242bb649ef96a3c8821cd8e22
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-05-18 08:58:14 +0200

    Update CustomSelectProps to allow label prop to accept ReactNode, enhancing flexibility in rendering options.

--- src/components/ui/select-base.tsx
+++ src/components/ui/select-base.tsx
@@ -10,7 +10,7 @@ interface CustomSelectProps {
   options: Option[];
   onChange: (value: string) => void;
   value?: string;
-  label?: string;
+  label?: string | ReactNode;
 }
 
 const SelectBase: React.FC<CustomSelectProps> = ({
