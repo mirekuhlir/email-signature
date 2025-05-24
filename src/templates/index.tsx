c775ba3ae06e824b8c0f541d09453d81d301c2c0
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-05-24 13:31:36 +0200

    signature_f

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -3,6 +3,7 @@ import { signature_b } from './signature_b';
 import { signature_c } from './signature_c';
 import { signature_d } from './signature_d';
 import { signature_e } from './signature_e';
+import { signature_f } from './signature_f';
 import { signature_empty_one_columns } from './signature_empty_one_columns';
 import { signature_empty_two_columns } from './signature_empty_two_columns';
 
@@ -13,6 +14,7 @@ export const getTemplates = () => {
     signature_c(),
     signature_d(),
     signature_e(),
+    signature_f(),
     signature_empty_one_columns(),
     signature_empty_two_columns(),
   ];
