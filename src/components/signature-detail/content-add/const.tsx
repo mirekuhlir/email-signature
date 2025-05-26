a01e36fb4709a4e8054d19adbc003d0fdd7e420c
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-05-26 19:59:36 +0200

    refactor

--- src/components/signature-detail/content-add/const.tsx
+++ src/components/signature-detail/content-add/const.tsx
@@ -10,39 +10,39 @@ import {
 export const CONTENT_TYPES = [
   {
     name: 'text',
-    description: 'plain text - can be anything (e.g. name, title, company)',
+    description: 'Plain text – can be anything (e.g. name, title, company).',
     content: getExampleText(),
     type: ContentType.TEXT,
   },
   {
     name: 'image',
-    description: 'image - can be anything (e.g. logo, photo, etc.)',
+    description: 'Image – can be anything (e.g. logo, photo, etc.).',
     content: 'image',
     type: ContentType.IMAGE,
   },
   {
     name: 'e-mail',
-    description: 'any email address (e.g. john.doe@company.com)',
+    description: 'Any email address (e.g. john.doe@company.com).',
     content: `${getEmailTextExample().prefix}${getEmailTextExample().email}`,
     type: ContentType.EMAIL,
   },
   {
     name: 'phone',
-    description: 'phone number (e.g. +1 123 456 7890)',
+    description: 'Phone number (e.g. +1 123 456 7890).',
     content: `${getPhoneTextExample().prefix}${getPhoneTextExample().phone}`,
     type: ContentType.PHONE,
   },
   {
     name: 'website',
     description:
-      'any website or link (e.g. company website, Instagram, LinkedIn, Facebook, etc.)',
+      'Any website or link (e.g. company website, Instagram, LinkedIn, Facebook, etc.).',
     content: `${getWebsiteTextExample().prefix}${getWebsiteTextExample().website}`,
     type: ContentType.WEBSITE,
   },
   {
     name: 'text with prefix',
     description:
-      'text that consists of two parts, each part can have its own style (color, font size, font weight, etc.)',
+      'Text that consists of two parts. Each part can have its own style (color, font size, font weight, etc.). For example, your name and position on one line, each in a different color.',
     content: `${getCustomValueTextExample().prefix}${getCustomValueTextExample().value}`,
     type: ContentType.CUSTOM_VALUE,
   },
