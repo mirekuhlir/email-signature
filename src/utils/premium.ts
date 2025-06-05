5fbb2b2aee2f34371b568c7047ad5f1dd3faaba5
Miroslav Uhlir <mirek.uhlir@gmail.com>
2025-06-05 21:57:42 +0200

    refactor

--- src/utils/premium.ts
+++ src/utils/premium.ts
@@ -1,8 +1,8 @@
 import { User } from "@supabase/supabase-js";
 
 export const getIsPremium = async (user: User) => {
-    const validFrom = user.app_metadata.premium.validFrom;
-    const validTo = user.app_metadata.premium.validTo;
+    const validFrom = user.app_metadata.premium?.validFrom;
+    const validTo = user.app_metadata.premium?.validTo;
 
     const isPremium = validFrom &&
         new Date(validFrom) < new Date() &&
